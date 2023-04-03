1ab41a54553c990cb39dadbea0d8970c90c0bb96
LightSpectr <maked440@gmail.com>
2023-04-03 16:35:22 +0200

    done function

--- cv1/app/src/main/java/com/example/bmi/MainActivity.java
+++ cv1/app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.app.AlertDialog;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.buttonCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                calc();
+            }
+        });
+
     }
+    private void calc(){
+        try {
+            EditText weight = (EditText) findViewById(R.id.editTextTextWeight);
+            EditText height = (EditText) findViewById(R.id.editTextTextHeight);
+
+
+            TextView res = (TextView) findViewById(R.id.textViewRes);
+            double w = Double.parseDouble(weight.getText().toString());
+            double h = Double.parseDouble(height.getText().toString()) * 0.01;
+            res.setText(String.format("%.2f", (w / ( h * h) )));
+        }
+        catch (NumberFormatException e) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("wrong format")
+
+
+                    .setPositiveButton(android.R.string.yes, null)
+
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+    }
+
+
 }
